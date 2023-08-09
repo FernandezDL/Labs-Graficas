50f230f935c4e7f47759ecb58ad1c83c628bc9e0
FernandezDL <dianaferbandez31@gmail.com>
2023-08-08 20:28:35 -0600

    Laboratorio terminado
    Archivos completos, incluyendo los 4 outputs (dentro de la carpeta "photoshoot")

---
+++ shaders.py
@@ -0,0 +1,109 @@
+import numpy as np
+import mathLib as ml
+
+def vertexShader(vertex, **kwargs):  
+    # El Vertex Shader se lleva a cabo por cada v�rtice
+
+    modelMatrix = kwargs["modelMatrix"]
+    viewMatrix= kwargs["viewMatrix"]
+    projectionMatrix= kwargs["projectionMatrix"]
+    vpMatrix= kwargs["vpMatrix"]
+
+    vt = [vertex[0],
+          vertex[1],
+          vertex[2],
+          1]
+
+    vt1= ml.multi4x4matrix(vpMatrix, projectionMatrix)
+    vt2= ml.multi4x4matrix(vt1, viewMatrix)
+    vt3= ml.multi4x4matrix(vt2, modelMatrix)
+    vt = ml.multimatrixvec(vt3, vt)
+
+    """ vt = vt.tolist()[0] """
+
+    vt = [vt[0]/vt[3],
+          vt[1]/vt[3],
+          vt[2]/vt[3]]
+
+    return vt
+
+def fragmentShader(**kwargs):
+    # El Fragment Shader se lleva a cabo por cada pixel
+    # que se renderizar� en la pantalla.
+
+    texCoords = kwargs["texCoords"]
+    texture = kwargs["texture"]
+
+    if texture != None:
+        color = texture.getColor(texCoords[0], texCoords[1])
+    else:
+        color = (1,1,1)
+
+
+    return color
+
+def flatShader(**kwargs):
+    dLight = kwargs["dLight"]
+    normal= kwargs["triangleNormal"]
+    texCoords = kwargs["texCoords"]
+    texture = kwargs["texture"]
+
+    b= 1.0
+    g= 1.0
+    r= 1.0
+
+    if texture != None:
+        textureColor = texture.getColor(texCoords[0], texCoords[1])    
+        b *= textureColor[2]
+        g *= textureColor[1]
+        r *= textureColor[0]
+
+    """dLight= np.array(dLight)"""
+    intensity= ml.dot_product(normal, -dLight)
+    
+    b *= intensity
+    g *= intensity
+    r *= intensity
+
+    if intensity > 0:
+        return r, g, b
+
+    else:
+        return [0,0,0]
+    
+def gouradShader(**kwargs):
+    texture= kwargs["texture"]
+    tA, tB, tC= kwargs["texCoords"]
+    nA, nB, nC= kwargs["normals"]
+    dLight = kwargs["dLight"]
+    u, v, w= kwargs["bCoords"]
+
+    b= 1.0
+    g= 1.0
+    r= 1.0
+
+    if texture != None:
+        tU= u * tA[0] + v * tB[0] + w * tC[0]
+        tV= u * tA[1] + v * tB[1] + w * tC[1]
+        
+        textureColor = texture.getColor(tU, tV)    
+        b *= textureColor[2]
+        g *= textureColor[1]
+        r *= textureColor[0]
+
+    normal= [u * nA[0] + v * nB[0] + w * nC[0],
+             u * nA[1] + v * nB[1] + w * nC[1],
+             u * nA[2] + v * nB[2] + w * nC[2]]
+    
+    dLight= np.array(dLight)
+    intensity= ml.dot_product(normal, -dLight)
+    
+    b *= intensity
+    g *= intensity
+    r *= intensity
+
+    if intensity > 0:
+        return r, g, b
+
+    else:
+        return [0,0,0]
