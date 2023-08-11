9547cc9c0f9a68ce9b4d60b78c9909538c18e640
FernandezDL <dianaferbandez31@gmail.com>
2023-08-10 22:31:10 -0600

    Agregando Shaders
    Agregando código de shaders de ejemplo y el primero perteneciente al lab2

--- Rasterizer2023.py
+++ Rasterizer2023.py
@@ -1,4 +1,4 @@
-from gl import Renderer
+from gl import Renderer, Model
 import shaders
 
 # El tama�o del FrameBuffer
@@ -10,14 +10,14 @@ rend = Renderer(width, height)
 
 # Le damos los shaders que se utilizar�s
 rend.vertexShader = shaders.vertexShader
-rend.fragmentShader = shaders.gouradShader
+rend.fragmentShader = shaders.emissionShader
 
 #Medium Shoot
 # Cargamos los modelos que rederizaremos
 rend.glLoadModel(filename = "models/SewingMachine.obj",
                  textureName = "textures/wood.bmp",
-                 translate = (0, -0.3, -2),
-                 rotate = (0, 90, 0),
+                 translate = (-0.15, -0.3, -2),
+                 rotate = (0, 0, 0),
                  scale = (3,3,3)) 
 
 # Se renderiza la escena
@@ -25,65 +25,3 @@ rend.glRender()
 
 # Se crea el FrameBuffer con la escena renderizada
 rend.glFinish("photoshoot/mediumAngle.bmp")
-
-#---------------------------------------------------------
-
-#Low Angle Shoot
-""" rend.glLookAt(camPos=(0, -1.5, 0),
-              eyePos=(0,2,-5))
-
-# Cargamos los modelos que rederizaremos
-rend.glLoadModel(filename = "models/SewingMachine.obj",
-                 textureName = "textures/wood.bmp",
-                 translate = (-0.1, -0.5, -2),
-                 rotate = (0, 0, 0),
-                 scale = (3,3,3))  
-                 
-# Se renderiza la escena
-rend.glRender()
-
-# Se crea el FrameBuffer con la escena renderizada
-rend.glFinish("photoshoot/lowAngle.bmp")
-
-#---------------------------------------------------------
-"""
-
-#High Angle Shoot
-""" rend.glLookAt(camPos=(0, 1.5, 0),
-              eyePos=(0,-2,-5))
-
-# Cargamos los modelos que rederizaremos
-rend.glLoadModel(filename = "models/SewingMachine.obj",
-                 textureName = "textures/wood.bmp",
-                 translate = (-0.1, -0.5, -2),
-                 rotate = (0, 0, 0),
-                 scale = (3,3,3)) 
-                 
-# Se renderiza la escena
-rend.glRender()
-
-# Se crea el FrameBuffer con la escena renderizada
-rend.glFinish("photoshoot/highAngle.bmp")
-
-#---------------------------------------------------------
-"""
-
-#Dutch Angle Shoot
-""" rend.glLookAt(camPos=(0, 1.5, 0),
-              eyePos=(0,-2,-5))
-
-# Cargamos los modelos que rederizaremos
-rend.glLoadModel(filename = "models/SewingMachine.obj",
-                 textureName = "textures/wood.bmp",
-                 translate = (0, -0.2, -2),
-                 rotate = (-20, 20, 25),
-                 scale = (3,3,3)) 
-                 
-# Se renderiza la escena
-rend.glRender()
-
-# Se crea el FrameBuffer con la escena renderizada
-rend.glFinish("photoshoot/dutchAngle.bmp")
-
-#---------------------------------------------------------
-"""
--- shaders.py
+++ shaders.py
@@ -44,7 +44,7 @@ def fragmentShader(**kwargs):
 
 def flatShader(**kwargs):
     dLight = kwargs["dLight"]
-    normal= kwargs["triangleNormal"]
+    normal= kwargs["normals"]
     texCoords = kwargs["texCoords"]
     texture = kwargs["texture"]
 
@@ -107,3 +107,186 @@ def gouradShader(**kwargs):
 
     else:
         return [0,0,0]
+    
+def toonShader(**kwargs):
+    texture= kwargs["texture"]
+    tA, tB, tC= kwargs["texCoords"]
+    nA, nB, nC= kwargs["normals"]
+    dLight = kwargs["dLight"]
+    u, v, w= kwargs["bCoords"]
+
+    b= 1.0
+    g= 1.0
+    r= 1.0
+
+    if texture != None:
+        tU= u * tA[0] + v * tB[0] + w * tC[0]
+        tV= u * tA[1] + v * tB[1] + w * tC[1]
+        
+        textureColor = texture.getColor(tU, tV)    
+        b *= textureColor[2]
+        g *= textureColor[1]
+        r *= textureColor[0]
+
+    normal= [u * nA[0] + v * nB[0] + w * nC[0],
+            u * nA[1] + v * nB[1] + w * nC[1],
+            u * nA[2] + v * nB[2] + w * nC[2]]
+    
+    dLight= np.array(dLight)
+    intensity= ml.dot_product(normal, -dLight)
+
+    if intensity <= 0.25:
+        intensity= 0.2
+
+    elif intensity <=0.5:
+        intensity= 0.45
+
+    elif intensity <=0.75:
+        intensity=0.7
+
+    elif intensity<=1.0:
+        intensity= 0.95
+    
+    b *= intensity
+    g *= intensity
+    r *= intensity
+
+    if intensity > 0:
+        return r, g, b
+
+    else:
+        return [0,0,0]
+    
+def redShader(**kwargs):
+    texture= kwargs["texture"]
+    tA, tB, tC= kwargs["texCoords"]
+    nA, nB, nC= kwargs["normals"]
+    dLight = kwargs["dLight"]
+    u, v, w= kwargs["bCoords"]
+
+    b= 1.0
+    g= 1.0
+    r= 1.0
+
+    if texture != None:
+        tU= u * tA[0] + v * tB[0] + w * tC[0]
+        tV= u * tA[1] + v * tB[1] + w * tC[1]
+        
+        textureColor = texture.getColor(tU, tV)    
+        b *= textureColor[2]
+        g *= textureColor[1]
+        r *= textureColor[0]
+
+    normal= [u * nA[0] + v * nB[0] + w * nC[0],
+             u * nA[1] + v * nB[1] + w * nC[1],
+             u * nA[2] + v * nB[2] + w * nC[2]]
+    
+    dLight= np.array(dLight)
+    intensity= ml.dot_product(normal, -dLight)
+    
+    b *= intensity
+    g *= intensity
+    r *= intensity
+
+    red= (1,0,0)
+
+    b *= red[2]
+    g *= red[1]
+    r *= red[0]
+
+    if intensity > 0:
+        return r, g, b
+
+    else:
+        return [0,0,0]
+    
+""" def yellowGlowShader(**kwargs):
+    texture= kwargs["texture"]
+    tA, tB, tC= kwargs["texCoords"]
+    nA, nB, nC= kwargs["normals"]
+    dLight = kwargs["dLight"]
+    u, v, w= kwargs["bCoords"]
+    camMatrix= kwargs["camMatrix"]
+
+    b= 1.0
+    g= 1.0
+    r= 1.0
+
+    if texture != None:
+        tU= u * tA[0] + v * tB[0] + w * tC[0]
+        tV= u * tA[1] + v * tB[1] + w * tC[1]
+        
+        textureColor = texture.getColor(tU, tV)    
+        b *= textureColor[2]
+        g *= textureColor[1]
+        r *= textureColor[0]
+
+    normal= [u * nA[0] + v * nB[0] + w * nC[0],
+             u * nA[1] + v * nB[1] + w * nC[1],
+             u * nA[2] + v * nB[2] + w * nC[2]]
+    
+    dLight= np.array(dLight)
+    intensity= ml.dot_product(normal, -dLight)
+
+    if intensity<= 0:
+        intensity=0
+    
+    b *= intensity
+    g *= intensity
+    r *= intensity
+
+    camForward= (camMatrix.item(0,2),
+                 camMatrix.item(1,2),
+                 camMatrix.item(2,2))
+    
+    glowAmount= 1- ml.dot_product(normal, camForward)
+
+    if glowAmount<=0: 
+        glowAmount=0
+
+    yellow= (1,1,0)
+
+    b += glowAmount * yellow[2]
+    g += glowAmount * yellow[1]
+    r += glowAmount * yellow[0]
+
+    if b>=1: 
+        b=1.0
+
+    if g>=1: 
+        g=1.0
+
+    if r>=1: 
+        r=1.0
+
+    return r, g, b """
+
+def emissionShader(**kwargs):
+    texture = kwargs["texture"]
+    tA, tB, tC = kwargs["texCoords"]
+    u, v, w= kwargs["bCoords"]
+
+    b = 0.0
+    g = 0.0
+    r = 0.0
+
+    if texture is not None:
+        tU = u * tA[0] + v * tB[0] + w * tC[0]
+        tV = u * tA[1] + v * tB[1] + w * tC[1]
+        
+        textureColor = texture.getColor(tU, tV)    
+        b += textureColor[2]
+        g += textureColor[1]
+        r += textureColor[0]
+
+    # Simulate emission by adding a constant value
+    emission_color = [0.2, 0.4, 0.8]  # Emission color
+    b += emission_color[2]
+    g += emission_color[1]
+    r += emission_color[0]
+
+    b = min(b, 1.0)
+    g = min(g, 1.0)
+    r = min(r, 1.0)
+
+    return r, g, b
