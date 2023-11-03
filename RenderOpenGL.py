824b312223efb6a48a07db6f8a044fb537839390
FernandezDL <dianaferbandez31@gmail.com>
2023-11-02 19:35:35 -0600

    final

---
+++ RenderOpenGL.py
@@ -0,0 +1,80 @@
+import pygame
+import glm
+from pygame.locals import *
+from gl import Renderer
+from model import Model
+from shaders import *
+
+width = 960
+height = 540
+
+pygame.init()
+
+screen = pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
+clock= pygame.time.Clock()
+
+rend= Renderer(screen)
+rend.setShader(vertex_shader, fragment_shader)
+
+#                 POSICIONES         U, V            NORMALS
+""" triangleData= [-0.5, -0.5, 0.0,   0.0, 0.0,     0.0, 0.0, 1.0,
+               -0.5,  0.5, 0.0,   0.0, 1.0,     0.0, 0.0, 1.0,
+                0.5, -0.5, 0.0,   1.0, 0.0,     0.0, 0.0, 1.0,
+                
+               -0.5,  0.5, 0.0,   0.0, 1.0,     0.0, 0.0, 1.0,
+                0.5,  0.5, 0.0,   1.0, 1.0,     0.0, 0.0, 1.0,
+                0.5, -0.5, 0.0,   1.0, 0.0,     0.0, 0.0, 1.0]
+
+triangleModel = Model(triangleData)
+triangleModel.loadTexture("textures/flores.jpg")
+triangleModel.position.z = -5
+triangleModel.scale = glm.vec3(2,2,2)
+
+rend.scene.append(triangleModel) """
+
+obj= rend.loadModel(filename= "models/turtle.obj", 
+                    texture="textures/turtle.jpg", 
+                    position = (0,0, -6),
+                    rotation = (-90, 0, 75),
+                    scale = (0.3,0.3,0.3))
+
+isRunning= True
+
+while isRunning:
+    deltaTime = clock.tick(60)/ 1000
+    keys= pygame.key.get_pressed()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isRunning = False
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                isRunning = False
+
+    if keys[K_d]:
+        rend.camPosition.x -= 5 * deltaTime
+
+    elif keys[K_a]:
+        rend.camPosition.x += 5 * deltaTime
+
+    if keys[K_w]:
+        rend.camPosition.y -= 5 * deltaTime
+        
+    elif keys[K_s]:
+        rend.camPosition.y += 5 * deltaTime
+
+    if keys[K_e]:
+        rend.camPosition.z -= 5 * deltaTime
+         
+    elif keys[K_q]:
+        rend.camPosition.z += 5 * deltaTime
+
+    obj.rotation.x += 45 * deltaTime
+
+    rend.elapsedTime += deltaTime
+    rend.render()
+
+    pygame.display.flip()
+
+pygame.quit()
---
+++ shaders.py
@@ -0,0 +1,38 @@
+vertex_shader = '''
+#version 450 core
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec2 texCoords;
+layout(location = 2) in vec3 normals;
+
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
+uniform float time;
+
+out vec2 UVs;
+out vec3 outNormals;
+
+void main(){
+    vec4 newPos = vec4(position.x, position.y, position.z, 1.0);
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * newPos;
+    UVs= texCoords;
+    outNormals= (modelMatrix * vec4(normals, 0.0)).xyz;
+}
+'''
+
+fragment_shader = ''' 
+#version 450 core
+
+layout (binding = 0) uniform sampler2D tex;
+
+in vec2 UVs;
+in vec3 outNormals;
+out vec4 fragColor;
+
+void main(){
+    
+    fragColor= texture(tex, UVs) ;
+}
+'''
